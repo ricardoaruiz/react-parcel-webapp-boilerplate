2922e60b52075bc87c3ff9d131de42be5046314d
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-11-26 12:06:14 -0300

    feat: example how to use images png jpg jpeg and gif with parcel

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import React from 'react'
 
 import { Button } from './components'
 
+import reactLogoPng from './assets/images/react-logo.png'
+import reactLogoJpg from './assets/images/react-logo.jpg'
+const reactLogoJpeg = new URL(
+  './assets/images/react-logo.jpeg?as=webp&width=40',
+  import.meta.url
+);
+const reactLogoGif = new URL(
+  './assets/images/react-logo.gif?as=webp&width=40',
+  import.meta.url
+);
+
 const App = () => {
   return (
     <div>
       App  
-      <Button>Clique aqui!</Button>    
+      <Button>Clique aqui!</Button>
+      <img src={reactLogoPng} alt="react logo png" style={{ width: '40px' }}/>
+      <img src={reactLogoJpg} alt="react logo jpg" style={{ width: '40px' }}/>
+      <img src={reactLogoJpeg.toString()} alt="react logo jpeg" style={{ width: '40px' }}/>
+      <img src={reactLogoGif.toString()} alt="react logo gif" style={{ width: '40px' }}/>
     </div>
   )
 }
